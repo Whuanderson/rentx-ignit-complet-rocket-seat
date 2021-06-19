ffcd9fcbbc450d59de9a5571f456b48044908d46
Whuanderson <whuandersonmsn@hotmail.com>
2021-06-19 14:41:18 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+import { StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { api } from '../../services/api';
+import { CarDTO } from '../../dtos/CarDTO';
+
+import { Car } from '../../components/Car';
+import { Load } from '../../components/Load';
+
+import {
+  Container,
+  Header,
+  HeaderContent,
+  TotalCars,
+  CarList,
+  MyCarsButton,
+} from './styles';
+
+export function Home() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const navigation = useNavigation();
+  const theme = useTheme();
+
+  function handleCarDetails(car: CarDTO) {
+    navigation.navigate('CarDetails', { car })
+  }
+
+  function handleOpenMyCars() {
+    navigation.navigate('MyCars')
+  }
+
+  useEffect(() => {
+    async function feetchCars() {
+      try {
+        const response = await api.get('/cars')
+        setCars(response.data);
+
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    feetchCars();
+  }, []);
+
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 Carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+      {loading ? <Load /> :
+        <CarList
+          data={cars}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) =>
+            <Car data={item} onPress={() => handleCarDetails(item)} />}
+        />
+      }
+
+      <MyCarsButton onPress={handleOpenMyCars}>
+        <Ionicons
+          name="ios-car-sport"
+          size={32}
+          color={theme.colors.shape}
+        />
+      </MyCarsButton>
+    </Container>
+  );
+}
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components'
+
+import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+import {
+  Calendar,
+  DayProps,
+  generateInterval,
+  MarkedDatesProps,
+} from '../../components/Calendar';
+
+import ArrowSvg from '../../assets/arrow.svg'
+import { CarDTO } from '../../dtos/CarDTO';
+import { getPlataformDate } from '../../utils/getPlataformDate';
+
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+interface RentalPeriod {
+  startFormatted: string;
+  endFormatted: string;
+}
+
+interface Params {
+  car: CarDTO;
+}
+
+export function Scheduling() {
+  const [lastSelectedDate, setLastSelectedDate] = useState<DayProps>({} as DayProps);
+  const [markedDates, setMarkedDates] = useState<MarkedDatesProps>({} as MarkedDatesProps);
+  const [rentalPeriod, setRentalPeriod] = useState<RentalPeriod>({} as RentalPeriod);
+
+  const theme = useTheme();
+  const navigation = useNavigation();
+  const route = useRoute();
+  const { car } = route.params as Params;
+
+  function handleConfirmRental() {
+    navigation.navigate('SchedulingDetails', {
+      car,
+      dates: Object.keys(markedDates)
+    });
+  }
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleChangeDate(date: DayProps) {
+    let start = !lastSelectedDate.timestamp ? date : lastSelectedDate;
+    let end = date;
+
+    if (start.timestamp > end.timestamp) {
+      start = end;
+      end = start;
+    }
+
+    setLastSelectedDate(end);
+    const interval = generateInterval(start, end);
+    setMarkedDates(interval);
+
+    const fristDate = Object.keys(interval)[0];
+    const endDate = Object.keys(interval)[Object.keys(interval).length - 1];
+
+    setRentalPeriod({
+      startFormatted: format(getPlataformDate(new Date(fristDate)), 'dd/MM/yyyy'),
+      endFormatted: format(getPlataformDate(new Date(endDate)), 'dd/MM/yyyy'),
+    })
+  }
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton
+          onPress={handleBack}
+          color={theme.colors.shape}
+        />
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={!!rentalPeriod.startFormatted}>
+              {rentalPeriod.startFormatted}
+            </DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÈ</DateTitle>
+            <DateValue selected={!!rentalPeriod.endFormatted}>
+              {rentalPeriod.endFormatted}
+            </DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content>
+        <Calendar
+          markedDate={markedDates}
+          onDayPress={handleChangeDate}
+        />
+      </Content>
+
+      <Footer>
+        <Button
+          title="Confirmar"
+          onPress={handleConfirmRental}
+          enabled={!!rentalPeriod.startFormatted}
+        />
+      </Footer>
+
+    </Container>
+  );
+}
