5a9a37275b63e13be44081521663c02c5d1ab7fe
Whuanderson <whuandersonmsn@hotmail.com>
2021-06-29 11:16:36 -0300

    Adicionado tela de loguin e cadastro. Criação de novos componentes. validação do cadastro

---
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
+import {
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  Keyboard,
+  Alert
+} from 'react-native';
+
+import { Confirmation } from '../../Confirmation';
+import { BackButton } from '../../../components/BackButton';
+import { Button } from '../../../components/Button';
+import { Bullet } from '../../../components/Bullet';
+import { PasswordInput } from '../../../components/PasswordInput';
+
+import {
+  Container,
+  Header,
+  Steps,
+  Title,
+  Subtitle,
+  Form,
+  FormTitle
+} from './styles';
+
+interface Params {
+  user: {
+    name: string;
+    email: string;
+    driverLicense: string;
+  }
+}
+
+export function SignUpSecondStep() {
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const navigation = useNavigation();
+  const route = useRoute();
+  const theme = useTheme();
+
+  const { user } = route.params as Params
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  function handleRegister() {
+    if(!password || !passwordConfirm){
+      return Alert.alert('Informe a senha e a confirmação');
+    }
+
+    if(password != passwordConfirm){
+      return Alert.alert('Verifique se as senhas estão iguais');
+    }
+    
+    navigation.navigate('Confirmation', {
+      nextScreenRoute: 'SignIn',
+      title: 'Conta criada!',
+      message: `Agora é so fazer login\ne aproveitar.`
+    })
+  }
+
+  return (
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <Header>
+            <BackButton onPress={handleBack} />
+            <Steps>
+
+              <Bullet />
+              <Bullet active />
+            </Steps>
+          </Header>
+
+          <Title>
+            Crie sua{'\n'}
+            conta
+          </Title>
+          <Subtitle>
+            Faça seu cadastro de{'\n'}
+            forma rápida e fácil
+          </Subtitle>
+
+          <Form>
+            <FormTitle>2. Senha</FormTitle>
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Senha"
+              onChangeText={setPassword}
+              value={password}
+            />
+            <PasswordInput 
+              iconName="lock"
+              placeholder="Repetir senha"
+              onChangeText={setPasswordConfirm}
+              value={passwordConfirm}
+            />
+          </Form>
+
+          <Button
+            color={theme.colors.sucess}
+            title="Cadastrar"
+            onPress={handleRegister}
+          />
+
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
